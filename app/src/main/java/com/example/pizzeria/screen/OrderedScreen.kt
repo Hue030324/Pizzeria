7fe86428524807244ea40d48bac30811b5e66468
Huệ <137085692+Hue030324@users.noreply.github.com>
2024-06-07 01:50:46 +0700

    Update OrderedScreen.kt

--- app/src/main/java/com/example/pizzeria/screen/OrderedScreen.kt
+++ app/src/main/java/com/example/pizzeria/screen/OrderedScreen.kt
@@ -10,6 +10,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.BottomAppBar
@@ -23,6 +25,7 @@ import androidx.compose.material.FloatingActionButtonDefaults
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.DateRange
 import androidx.compose.material.icons.rounded.Favorite
 import androidx.compose.material.icons.rounded.Home
 import androidx.compose.material.icons.rounded.Person
@@ -30,6 +33,7 @@ import androidx.compose.material.icons.rounded.ShoppingCart
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -42,17 +46,23 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.example.pizzeria.R
 import com.example.pizzeria.nav.Screen
 import com.example.pizzeria.ui.theme.Shapes
+import com.example.pizzeria.ui.theme.blackcart
+import com.example.pizzeria.ui.theme.lightGray
 import com.example.pizzeria.ui.theme.yellow1
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun OrderedScreen(navController: NavHostController){
+    val scrollState = rememberLazyListState()
 
     Scaffold(
         scaffoldState = rememberScaffoldState(),
@@ -200,9 +210,177 @@ fun OrderedScreen(navController: NavHostController){
 
                 }
             )
+        },
+
+        content = {
+            LazyColumn(contentPadding = PaddingValues(bottom = 75.dp, start = 16.dp, end = 16.dp), state = scrollState){
+                item {
+                    OrderDetail()
+                }
+            }
         }
 
+    )
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun OrderDetail(){
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 10.dp, horizontal = 5.dp)
     ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = Icons.Rounded.DateRange,
+                contentDescription = null,
+                modifier = Modifier.height(20.dp)
+            )
+            Spacer(modifier = Modifier.width(5.dp))
+            Text(
+                text = "Friday, Jun 7, 2024",
+                fontWeight = FontWeight.Medium,
+                fontSize = 15.sp,
+                color = blackcart
+            )
+        }
 
+        Surface(
+            shape = RoundedCornerShape(12.dp),
+            color = Color.White,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    color = lightGray,
+                    shape = RoundedCornerShape(12.dp),
+                )
+                .padding(bottom = 2.dp),
+            shadowElevation = 6.dp,
+            onClick = {
+                //den trang sua/ them dia chi
+            }
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(15.dp)
+                    .padding(top = 15.dp),
+            ) {
+                Row(
+                    modifier = Modifier
+                        .padding(bottom = 15.dp)
+                ) {
+                    Text(
+                        text = "Name contact :",
+                        fontSize = 16.sp,
+                        color = Color.DarkGray
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(
+                        text = "Hoang Quynh",
+                        fontSize = 17.sp,
+                        color = blackcart,
+                        fontWeight = FontWeight.Medium
+                    )
+                }
+                Row(
+                    modifier = Modifier
+                        .padding(bottom = 15.dp)
+                ) {
+                    Text(
+                        text = "Phone number :",
+                        fontSize = 16.sp,
+                        color = Color.DarkGray
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(
+                        text = "0986753241",
+                        fontSize = 17.sp,
+                        color = blackcart,
+                        fontWeight = FontWeight.Medium
+                    )
+                }
+                Row(
+                    modifier = Modifier
+                        .padding(bottom = 15.dp)
+                ) {
+                    Text(
+                        text = "Address :",
+                        fontSize = 16.sp,
+                        color = Color.DarkGray
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(
+                        text = "365 Tran Dai Nghia, Hoa Hai, Ngu Hanh Son, Da Nang",
+                        fontSize = 17.sp,
+                        color = blackcart,
+                        fontWeight = FontWeight.Medium,
+                        lineHeight = 20.sp
+                    )
+                }
+                Row(
+                    modifier = Modifier
+                        .padding(bottom = 15.dp)
+                ) {
+                    Text(
+                        text = "Items :",
+                        fontSize = 16.sp,
+                        color = Color.DarkGray
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(
+                        text = "Smoked Salmon Pizza x1 \n" +
+                                "Special Royal Beef Burger x1 \n" +
+                                "Classic Fries x1 \n" +
+                                "Sprite x1",
+                        fontSize = 17.sp,
+                        color = blackcart,
+                        fontWeight = FontWeight.Medium,
+                        lineHeight = 25.sp
+                    )
+                }
+                Row(
+                    modifier = Modifier
+                        .padding(bottom = 15.dp)
+                ) {
+                    Text(
+                        text = "Time :",
+                        fontSize = 16.sp,
+                        color = Color.DarkGray
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(
+                        text = "11:49 PM",
+                        fontSize = 17.sp,
+                        color = blackcart,
+                        fontWeight = FontWeight.Medium
+                    )
+                }
+                Row(
+                    modifier = Modifier
+                        .padding(bottom = 15.dp)
+                ) {
+                    Text(
+                        text = "Total :",
+                        fontSize = 16.sp,
+                        color = Color.DarkGray
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(
+                        text = "$43.00",
+                        fontSize = 17.sp,
+                        color = blackcart,
+                        fontWeight = FontWeight.Medium
+                    )
+                }
+            }
+        }
     }
+}
+
+@Preview
+@Composable
+fun previewOrderedScreen(){
+    OrderedScreen(rememberNavController())
 }
